fef2c917918c03d3580d9f6803d85208048e45d7
Praisecodes <cmancoder@gmail.com>
2024-10-06 02:29:08 +0100

    chore: little adjustments

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -6,7 +6,7 @@ interface Props extends TextInputProps { }
 const Input = (props: Props) => {
   return (
     <TextInput
-      className={`border-[0.5px] border-grey-300 focus:border-accent py-4 px-4 rounded-[18px] text-white font-inter text-base`}
+      className={`border-[0.5px] border-grey-300 focus:border-accent pt-3.5 pb-5 px-4 rounded-[18px] text-white font-inter text-base`}
       placeholderTextColor={"#a3a3a3"}
       {...props}
     />
