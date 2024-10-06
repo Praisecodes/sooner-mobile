6f92a4fc29486f30eb362f917842b2c93d952367
Praisecodes <cmancoder@gmail.com>
2024-10-06 20:14:43 +0100

    chore: minor change

--- src/screens/app/dashboard/index.tsx
+++ src/screens/app/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, Image, TouchableWithoutFeedback, ScrollView } from 'react-native';
 import React from 'react';
 import RootLayout from '../../rootLayout';
 import StatsCard from '../../../components/dashboard/stats_card';
@@ -29,65 +29,67 @@ const Dashbaord = ({ navigation }: BottomTabScreenProps<AppTabsParamList, "dashb
 
   return (
     <RootLayout>
-      <View className={`px-5 py-4`}>
-        <StatsCard stats={stats} />
+      <ScrollView>
+        <View className={`px-5 py-4`}>
+          <StatsCard stats={stats} />
 
-        <View className={`flex flex-row items-center justify-center mt-8`}>
-          <View className={`flex-1 min-h-20 bg-grey-200 px-4 py-4 rounded-3xl`}>
-            <View className={`p-2 bg-grey-300 self-start rounded-lg`}>
-              <Image source={Streak} className={`w-6 h-6`} />
-            </View>
+          <View className={`flex flex-row items-center justify-center mt-8`}>
+            <View className={`flex-1 min-h-20 bg-grey-200 px-4 py-4 rounded-3xl`}>
+              <View className={`p-2 bg-grey-300 self-start rounded-lg`}>
+                <Image source={Streak} className={`w-6 h-6`} />
+              </View>
 
-            <Text className={`font-inter-semibold text-lg text-white mt-2`}>
-              Streak
-            </Text>
+              <Text className={`font-inter-semibold text-lg text-white mt-2`}>
+                Streak
+              </Text>
 
-            <Text className={`mt-8 text-white text-2xl font-inter-semibold`}>
-              17 days
-            </Text>
-          </View>
-
-          <View className={`w-2.5`} />
-
-          <View className={`flex-1 min-h-20 bg-grey-200 px-4 py-4 rounded-3xl`}>
-            <View className={`p-2 bg-grey-300 self-start rounded-lg`}>
-              <Image source={Average} className={`w-6 h-6`} />
-            </View>
-
-            <Text className={`font-inter-semibold text-lg text-white mt-2`}>
-              Daily average
-            </Text>
-
-            <Text className={`mt-8 text-white text-2xl font-inter-semibold`}>
-              1h 8m 45s
-            </Text>
-          </View>
-        </View>
-
-        <View className={`mt-8 bg-grey-200 p-5 rounded-3xl`}>
-          <View className={`flex flex-row items-center border-b border-b-grey-300 pb-3 mb-4`}>
-            <View className={`p-2 rounded-xl bg-grey-300`}>
-              <Image source={ProjectsIcon} className={`ios:w-7 ios:h-7 android:h-6 android:w-6`} />
-            </View>
-
-            <Text className={`text-white font-inter-semibold ios:text-2xl android:text-xl ml-3`}>
-              Projects
-            </Text>
-          </View>
-
-          {projects.map((project, index) => (
-            <ProjectCard project={project} key={index} />
-          ))}
-
-          <TouchableWithoutFeedback onPress={() => navigation.navigate("projects")}>
-            <View className={`rounded-full border border-grey-300 bg-grey-200 mt-4 py-4 px-5`}>
-              <Text className={`text-white text-center font-inter-semibold text-base`}>
-                See All Projects
+              <Text className={`mt-8 text-white text-2xl font-inter-semibold`}>
+                17 days
               </Text>
             </View>
-          </TouchableWithoutFeedback>
+
+            <View className={`w-2.5`} />
+
+            <View className={`flex-1 min-h-20 bg-grey-200 px-4 py-4 rounded-3xl`}>
+              <View className={`p-2 bg-grey-300 self-start rounded-lg`}>
+                <Image source={Average} className={`w-6 h-6`} />
+              </View>
+
+              <Text className={`font-inter-semibold text-lg text-white mt-2`}>
+                Daily average
+              </Text>
+
+              <Text className={`mt-8 text-white text-2xl font-inter-semibold`}>
+                1h 8m 45s
+              </Text>
+            </View>
+          </View>
+
+          <View className={`mt-8 bg-grey-200 p-5 rounded-3xl`}>
+            <View className={`flex flex-row items-center border-b border-b-grey-300 pb-3 mb-4`}>
+              <View className={`p-2 rounded-xl bg-grey-300`}>
+                <Image source={ProjectsIcon} className={`ios:w-7 ios:h-7 android:h-6 android:w-6`} />
+              </View>
+
+              <Text className={`text-white font-inter-semibold ios:text-2xl android:text-xl ml-3`}>
+                Projects
+              </Text>
+            </View>
+
+            {projects.map((project, index) => (
+              <ProjectCard project={project} key={index} />
+            ))}
+
+            <TouchableWithoutFeedback onPress={() => navigation.navigate("projects")}>
+              <View className={`rounded-full border border-grey-300 bg-grey-200 mt-4 py-4 px-5`}>
+                <Text className={`text-white text-center font-inter-semibold text-base`}>
+                  See All Projects
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </RootLayout>
   )
 }
